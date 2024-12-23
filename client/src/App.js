f2456672a0f95bfc7fb531ae216e2ea8e27e205f
ChayaCodes <chaya41182@gmail.com>
2024-12-23 20:03:16 +0200

    Revert "Merge pull request #49 from rivka14/Add_responsive_navBar"
    This reverts commit 63b19806e1284f9070e14ab772e1a7cbd43e85ed, reversing
    changes made to c2d0f087b9a095319fa915ef507c03613a2cb37e.

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,32 @@
 import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Home from "./pages/HomePage/HomePage";
 import Login from "./pages/LoginForm/LoginForm";
 import Register from "./pages/RegisterForm/RegisterForm";
 import CreateQuiz from "./pages/CreateQuiz/CreateQuiz";
 import QuizPage from "./pages/QuizPage/QuizPage";
 import Quizzes from "./pages/Quizzes/Quizzes";
+// import Container from "./components/Container/Container";
 import LeaderPlayers from './pages/LeaderPlayer/LeaderPlayer';
-import Layout from "./components/Layout/Layout"
-import NavBar from "./components/Navbar/NavBar";
-const App = () => {
+import Layout from "./components/Layout/Layout";
 
+function App() {
   return (
-    <>
-      <Router>
-        <NavBar />
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/login" element={<Login setUserName={setUserName} />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/create-quiz" element={<CreateQuiz />} />
-            <Route path="/quiz" element={<QuizPage userName={userName} />} />
-            <Route path="/quizzes" element={<Quizzes />} />
-            <Route path="/leader-player" element={<LeaderPlayers />} />
-          </Routes>
-        </Layout>
-      </Router>
-    </>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/create-quiz" element={<CreateQuiz />} />
+          <Route path="/quiz/:quizId" element={<QuizPage />} /> 
+          <Route path="/quizzes" element={<Quizzes />} />
+          <Route path="/leader-player" element={<LeaderPlayers />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,34 +1,18 @@
-import React from "react";
-import Sidebar from "../Sidebar/Sidebar";
+import React from 'react';
 import "./Layout.css";
+import Sidebar from '../Sidebar/Sidebar';
 import Footer from '../Footer/Footer'; 
 
-const Layout=({ children })=>{
-    return (
-         <>
-           {/* I kept the sidebar code so it can be restored if needed. */}
-           {/* <div className="container">
-           <Sidebar />
-           <div className="content">{children}</div>
-           </div> */}
+function Layout({ children }) {
+  return (
+    <div className="layout">
+      <div className="container">
+        <Sidebar />
+        <div className="content">{children}</div>
+      </div>
+      <Footer /> 
+    </div>
+  );
+}
 
-           <div className="content">{children}</div>
-
-           <footer className="footer">
-           <span>
-            פותח באהבה ❤️ על ידי חיה קרמר | צור קשר: chaya41182@gmail.com |
-            טלפון: 058-3241182
-           </span>
-           </footer>
-           <div className="layout">
-           <div className="container">
-           <Sidebar />
-           <div className="content">{children}</div>
-           </div>
-           <Footer /> 
-           </div>
-        </>
-      );
-  }
-  
-  export default Layout;
+export default Layout;
